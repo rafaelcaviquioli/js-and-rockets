404af42456661c02273649b6706317314cc8f0bf
Rafael Caviquioli <rafaelcitj@gmail.com>
2020-02-12 01:31:13 -0300

    Create missions filters

--- src/spacex-api/launches/filters.js
+++ src/spacex-api/launches/filters.js
@@ -1,6 +1,27 @@
-const filterMissionsOf2018WithNASAPayload = (missions) => {
-    return missions
-};
+
+const filterMissionsByYear = year => mission =>
+    parseInt(mission.launch_year) === parseInt(year);
+
+const filterMissionsByCustomerPayload = customerName => mission => {
+    const { payloads } = mission.rocket.second_stage;
+
+    for (let pCount = 0; pCount < payloads.length; pCount++) {
+        const { customers } = payloads[pCount];
+
+        for (let cCount = 0; cCount < customers.length; cCount++) {
+            const customer = customers[cCount];
+            if (customer.indexOf(customerName) > -1) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+const filterMissionsOf2018WithNASAPayload = missions => missions
+    .filter(filterMissionsByYear(2018))
+    .filter(filterMissionsByCustomerPayload("NASA"));
 
 module.exports = {
     filterMissionsOf2018WithNASAPayload
